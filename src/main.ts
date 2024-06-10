ed63b8e61920d9f35344d1a0548562fa8cf03685
fan9704 <cxz123499@gmail.com>
2024-06-10 15:11:44 +0800

    🐳 chore(Main): Inject Alert and AOS

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,11 @@ import router from '@/router';
 import { createVuestic } from "vuestic-ui";
 import "vuestic-ui/css";
 
+import VueSweetalert2 from 'vue-sweetalert2';
+import 'sweetalert2/dist/sweetalert2.min.css';
+
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 const vue = createApp(App);
 
@@ -20,6 +25,9 @@ vue.use(router);
 vue.use(store);
 vue.use(vuetify);
 // vue.use(createVuestic());
+vue.use(VueSweetalert2);
+// @ts-ignore
+vue.use(AOS.init());
 
 // Run!
 router
--- src/router.ts
+++ src/router.ts
@@ -9,7 +9,8 @@ import {
 } from 'vue-router';
 
 // Components
-import HomeView from '@/views/HomeView.vue';
+import PanelView from '@/views/PanelView.vue';
+import CreateRecordView from '@/views/network/CreateRecordView.vue';
 
 // Pinia Store
 
@@ -20,44 +21,60 @@ import HomeView from '@/views/HomeView.vue';
 const routes: RouteRecordRaw[] = [
   {
     path: '/',
-    name: 'Home',
-    component: HomeView,
+    name: 'Index',
+    component: async () => await import('@/views/IndexView.vue'),
+  },
+  {
+    path: '/network',
+    name: 'Network',
+    component: PanelView,
+  },
+  {
+    path: '/network/:id',
+    name: 'NetworkEditView',
+    component: async () => await import('@/views/network/NetworkEditView.vue'),
+    props: true,
   },
   {
     path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (About.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: async () => await import('@/views/AboutView.vue'),
+  },
+  {
+    path: '/new',
+    name: 'create',
+    component: CreateRecordView,
+  },
+  {
+    path: '/iptables',
+    name: 'Iptables',
+    component: async () => await import('@/views/network/IptablesView.vue'),
+  },
+  // PVE
+  {
+    path: '/pve/nodes',
+    name: 'PVENodes',
+    component: async () => await import('@/views/proxmox/NodesView.vue'),
+  },
+  {
+    path: '/pve/nodes/:node',
+    name: 'PVENode-Resources',
+    component: async () => await import('@/views/proxmox/NodeInfo.vue'),
+    props: true,
+  },
+  {
+    path: '/pve/cluster',
+    name: 'PVECluster-Resources',
+    component: async () => await import('@/views/proxmox/ClusterInfo.vue'),
   },
 ];
 
 /** Vue Router */
 const router: Router = createRouter({
-  /**
-   * History Mode
-   *
-   * @see {@link https://router.vuejs.org/guide/essentials/history-mode.html }
-   */
   history: createWebHistory(import.meta.env.BASE_URL), // createWebHashHistory(import.meta.env.BASE_URL)
-  /*
-  scrollBehavior: (to, _from, savedPosition) => {
-    let scrollTo: number | string = 0;
-
-    if (to.hash) {
-      scrollTo = to.hash;
-    } else if (savedPosition) {
-      scrollTo = savedPosition.top;
-    }
-    return goTo(scrollTo);
-  },
-  */
   routes,
 });
 
-// Global before guards
-// https://router.vuejs.org/guide/advanced/navigation-guards.html#global-before-guards}
 router.beforeEach(
   async (
     to: RouteLocationNormalized,
@@ -65,47 +82,12 @@ router.beforeEach(
     next: NavigationGuardNext
   ) => {
     const globalStore = useGlobal();
-    // Show Loading
-    // comment out for https://github.com/logue/vite-vuetify-ts-starter/issues/16
-    // globalStore.setLoading(true);
-
-    // Hide snack bar
     globalStore.setMessage('');
     next();
   }
 );
-
-// Global After Hooks
-// https://router.vuejs.org/guide/advanced/navigation-guards.html#global-after-hooks}
 router.afterEach(() => {
   const globalStore = useGlobal();
-  // Hide Loading
   globalStore.setLoading(false);
 });
-
-/*
-const scrollBehavior = async (
-  to: RouteLocationNormalized,
-  _from: RouteLocationNormalized,
-  savedPosition: RouteLocation
-): Promise<any> => {
-  let scrollpos = {};
-  if (to.hash) {
-    scrollpos = {
-      el: to.hash,
-      behavior: 'smooth',
-    };
-  } else if (savedPosition) {
-    scrollpos = savedPosition;
-  } else {
-    scrollpos = { top: 0 };
-  }
-  return await new Promise((resolve, _reject) => {
-    setTimeout(() => {
-      resolve(scrollpos);
-    }, 600);
-  });
-};
-*/
-
 export default router;
